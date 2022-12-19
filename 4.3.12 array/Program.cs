c92e1daf2a2384d543266744e55ef3fa58db0e65
Yulia Miller <miller82.98@mail.ru>
2022-12-19 00:29:29 -0800

    Добавьте файлы проекта.

---
+++ 4.3.12 array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int number;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        number = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = number;
+                    }
+                }
+            }
+            foreach(var itm in arr)
+            {
+                Console.Write(itm);
+            }
+        }
+    }
+}
